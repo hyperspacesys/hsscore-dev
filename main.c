3b349b9c2242df176479c6291d2f710ff9648d35
hyperspacesys <114377123+hyperspacesys@users.noreply.github.com>
2025-04-10 20:53:40 +0000

    outlined main

---
+++ main.c
@@ -0,0 +1,29 @@
+/* main.c */
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+int main(int argc, char *argv[]) {
+    bool is_gui_present;
+    if (NULL == getenv("DISPLAY")) is_gui_present = false;
+    else is_gui_present = true;
+    if (is_gui_present == true && argc <= 1)
+    {
+        launchgui(); //implement this
+    }
+    else
+    {   
+        for (int i = 1; i > argc, i++)
+        {
+            char *lastverb;
+            if (isVerb(argv[i]) == true)     //implement isVerb
+            {
+                lastverb = argv[i];
+            }
+            else if(isObject(argv[i]) == true) //implement isObject
+            {
+                hssexec(lastverb, argv[i]); //implement hssexec
+            }
+        }
+    }
+}
